7540af774504679452b22fb6c10b89c0940fa1c8
devlakunle <devlakunle@gmail.com>
2024-12-31 14:01:20 +0100

    feat: update UI and enhance handleDownload method
    - Improved the application UI with better design and usability.
    - Updated `handleDownload` method:
      - Added validation to check for empty or invalid URLs.
      - Integrated axios for POST requests to the backend API.
      - Implemented video download logic with blob creation and dynamic filename generation.
      - Added error handling for failed downloads.
      - Ensured cleanup of DOM elements and object URLs after download.
      - Reset state after successful download.
    - Installed Azios for making API request

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-gradient-to-b from-light to-light dark:from-dark dark:to-dark">
+  <div :class="[{'light-mode': !darkMode},{'dark-mode:': darkMode},'min-h-screen' ,'flex' ,'flex-col']" >
     <!-- Header -->
     <header class="px-6 py-4 shadow-md">
       <div class="flex justify-between items-center max-w-[960px] mx-auto">
@@ -63,21 +63,21 @@
     <main class="flex-grow container mt-3 mx-auto p-6 text-gray-700 dark:text-gray-400">
       <div class="text-center mb-8">
         <h2 class="rainbow dark:reverse-rainbow text-4xl font-bold">Download Videos Seamlessly</h2>
-        <p class="md:text-xl font-extrabold mt-6 font-Roboto">Download videos from your favorite platforms ✨</p>
+        <p class="md:text-xl font-extrabold mt-6 ">Download videos from your favorite platforms ✨</p>
       </div>
 
      <!-- Download Form -->
-      <form class="p-6 max-w-xl mx-auto">
+      <form v-on:submit.prevent class="p-6 max-w-xl mx-auto">
         <div class="mb-4 lg:text-lg text-md text-dark dark:text-gray-400">
           <label for="videoURL" class="block font-lg font-bold font-ethnocentric">Video URL :</label>
           <div class="relative">
             <input v-model="videoUrl" type="url" id="videoURL" placeholder="Paste video URL" class="placeholder:italic placeholder:text-slate-400 mt-1 block w-full p-5 rounded-md border border-4 border-tomato-800 shadow-xl focus:outline-none focus:ring-tomato-500 focus:ring-5 focus:border-tomato-500 dark:focus:border-tomato-500 dark:border-gray-700 dark:bg-gray-900 bg-gray-100 border broder-tomato-500" >
-            <span class="absolute inset-y-0 right-0 grid place-items-center w-[50px] cursor-pointer  my-1 mx-1 dark:bg-gray-900 bg-gray-100 shadow-2xl shadow-inner border-l-2">
+            <span class="absolute inset-y-0 right-0 grid place-items-center w-[50px] cursor-pointer  my-1 mx-1 dark:bg-gray-800 bg-gray-100 shadow-2xl shadow-inner border-l-1">
               <svg @click="handlePaste" v-if="!videoUrl" class="w-8 h-auto fill-slate-500 hover:fill-tomato-500 dark:hover:fill-tomato-500 transition-colors duration-300 ease-in-out"   fill="currentColor" width="800px" height="800px" viewBox="0 0 32 32" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:serif="http://www.serif.com/" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>paste</title>
                 <path d="M10,1c-1.326,-0 -2.598,0.527 -3.536,1.464c-0.937,0.938 -1.464,2.21 -1.464,3.536c0,5.322 -0,14.678 0,20c-0,1.326 0.527,2.598 1.464,3.536c0.938,0.937 2.21,1.464 3.536,1.464c3.486,0 8.514,0 12,0c1.326,0 2.598,-0.527 3.536,-1.464c0.937,-0.938 1.464,-2.21 1.464,-3.536l-0,-20c0,-1.326 -0.527,-2.598 -1.464,-3.536c-0.938,-0.937 -2.21,-1.464 -3.536,-1.464l-0,3c0,1.326 -0.527,2.598 -1.464,3.536c-0.938,0.937 -2.21,1.464 -3.536,1.464l-2,-0c-1.326,0 -2.598,-0.527 -3.536,-1.464c-0.937,-0.938 -1.464,-2.21 -1.464,-3.536l0,-3Zm1,25l10,0c0.552,0 1,-0.448 1,-1c0,-0.552 -0.448,-1 -1,-1l-10,0c-0.552,0 -1,0.448 -1,1c0,0.552 0.448,1 1,1Zm0,-6l10,0c0.552,0 1,-0.448 1,-1c0,-0.552 -0.448,-1 -1,-1l-10,0c-0.552,0 -1,0.448 -1,1c0,0.552 0.448,1 1,1Zm0,-6l10,0c0.552,0 1,-0.448 1,-1c0,-0.552 -0.448,-1 -1,-1l-10,0c-0.552,0 -1,0.448 -1,1c0,0.552 0.448,1 1,1Zm9,-13l-0,3c0,0.796 -0.316,1.559 -0.879,2.121c-0.562,0.563 -1.325,0.879 -2.121,0.879l-2,0c-0.796,0 -1.559,-0.316 -2.121,-0.879c-0.563,-0.562 -0.879,-1.325 -0.879,-2.121l-0,-3l8,-0Z"/><g id="Icon"/>
               </svg>
-              <span v-else @click="videoUrl=''" class="block hover:text-rose-500 drop-shadow-xl font-bold text-lg text-slate-500" title="clear">
+              <span v-else @click="handleClear" class="block hover:text-rose-500 drop-shadow-xl font-bold text-lg text-slate-500" title="clear">
                 <svg class=" w-8 h-auto drop-shadow-lg" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="cancelIconTitle" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" color="none">
                   <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title id="cancelIconTitle">Cancel</title> <path d="M15.5355339 15.5355339L8.46446609 8.46446609M15.5355339 8.46446609L8.46446609 15.5355339"></path> <path d="M4.92893219,19.0710678 C1.02368927,15.1658249 1.02368927,8.83417511 4.92893219,4.92893219 C8.83417511,1.02368927 15.1658249,1.02368927 19.0710678,4.92893219 C22.9763107,8.83417511 22.9763107,15.1658249 19.0710678,19.0710678 C15.1658249,22.9763107 8.83417511,22.9763107 4.92893219,19.0710678 Z"></path> </g>
                 </svg>
@@ -87,9 +87,10 @@
         </div>  
 
         <button
+          v-show="videoInfo.title"
           @click="handleDownload"
           :disabled="isLoading"
-          type="submit"
+          type="button"
           class="w-full md:block md:mx-auto md:max-w-[50%] p-4 text-white font-extrabold font-ethnocentric rounded-md bg-gradient-to-r from-tomato-500 to-red-900 hover:bg-gradient-to-l hover:from-red-500 hover:to-tomato-800  active:bg-gradient-to-r active:from-white active:to-tomato-700 transition duration-300 ease-in-out shadow-lg hover:shadow-xl disabled:opacity-50 disabled:cursor-not-allowed dark:hover:from-tomato-800 dark:hover:to-dark"
         >
           Download
@@ -97,12 +98,12 @@
       </form>
 
       <!-- Video Previews -->
-        <div v-show="videoUrl" class="mt-8 flex flex-col max-w-lg mx-auto h-auto bg-white dark:bg-gray-800 shadow-2xl rounded-lg overflow-hidden transform transition duration-300 hover:scale-105">
-          <iframe v-if="platform === 'youtube'" :src="embedUrl" frameborder="0" allowfullscreen class="w-full h-[250px] rounded-t-lg"></iframe>
-          <video v-else :src="videoUrl" controls class="w-full h-[250px] rounded-t-lg"></video>
-          <div class="p-4 bg-gradient-to-b from-tomato-500 to-tomato-800 text-gray-100">
-            <h3 class="text-lg font-bold">Video Title</h3>
-            <p class="text-sm">{{ platform }}</p>
+        <div v-show="videoInfo.title" class="mt-8 flex flex-col max-w-lg mx-auto h-auto bg-white dark:bg-gray-800 shadow-2xl rounded-lg overflow-hidden transform transition duration-300 hover:scale-105">
+          <iframe v-if="videoInfo.platform === 'youtube'" :src="embedUrl" frameborder="0" allowfullscreen class="w-full h-[250px] rounded-t-lg"></iframe>
+          <video v-else :src="videoInfo.url" class="w-full h-[250px] rounded-t-lg"></video>
+          <div class="p-4 bg-gradient-to-l from-tomato-500 via-tomato-800 to-red-900 ">
+            <h3 class="text-sm text-light font-bold  font-ethnocentric">Video Title: <span class="font-aleo text-gray-200 italics">{{ videoInfo.title }}</span></h3>
+            <p class="text-sm font-ethnocentric text-light mt-3">Platform: <span class="font-aleo text-gray-200 capitalize"> {{ videoInfo.platform }} </span></p>
           </div>
         </div>
 
@@ -131,6 +132,7 @@
   import LoadingSpinner from "./components/LoadingSpinner.vue";
   import SuccessAnimation from "./components/SuccessAnimation.vue";
   import { ref } from 'vue';
+  import axios from 'axios';
   
   export default {
     components: {
@@ -152,39 +154,83 @@
         isSuccess: false,
         darkMode: false,
         platform: "",
+        videoInfo: {},
       };
     },
 
     methods: {
-      handleInput() {
-        if (this.isLoading) return;
-        this.isSuccess = false;
-        this.errorMessage = "";
-      },
-
-      handleDownload(e) {
+      async handleDownload(e) {
         e.preventDefault()
         if (!this.videoUrl.trim()) {
           this.errorMessage = "Please enter a valid URL!";
           return;
         }
-        this.isLoading = true;
 
-        // Simulate API call
-        setTimeout(() => {
-          this.isLoading = false;
-          if (Math.random() > 0.5) {
-            this.isSuccess = true;
-            this.videoUrl = "";
-          } else {
-            this.errorMessage = "Download failed. Please try again.";
+        this.isLoading = true
+
+        try{
+          const response = await axios.post(
+            "http://127.0.0.1:8000/social_vidz/api/download", 
+            {
+              url: this.videoInfo.url.trim(),
+              platform: this.videoInfo.platform
+            },
+            {responseType: "blob"}
+          )
+
+          if(!response.status === 200) {
+            throw new Error(response.data.data.detail)
           }
-        }, 3000);
+
+          console.log(response.data)
+
+          const blob = new Blob([response.data], {type: "video/mp4"})
+          const downloadLink = document.createElement("a");
+          const sanitizedTitle = this.videoInfo.title.trim().replace(/\s+/g, "_").substr(0, 15);
+          downloadLink.download = `${sanitizedTitle || "video"}.mp4`;
+          downloadLink.href = URL.createObjectURL(blob);
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+
+          // Clean up
+          document.body.removeChild(downloadLink);
+          URL.revokeObjectURL(downloadLink.href);
+          this.handleClear()
+          this.isSuccess = true;
+
+
+        }catch (err) {
+          console.error("Download error:", err);
+          this.errorMessage = "Failed to download video.";
+        }finally {
+          this.isLoading = false
+        }       
       },
 
       toggleDarkMode() {
         this.darkMode = !this.darkMode
         document.documentElement.classList.toggle("dark", this.darkMode)
+      },
+
+      async getVideoInfo(url) {
+        this.isLoading = true
+        try {
+          const response = await axios(`http://127.0.0.1:8000/social_vidz/api/get_info?url=${url}`);  
+          if (!response.status === 200){
+            console.error(response.data.data || "cdsdvdsdssddssv")
+            throw new Error(response.data.data.detail)
+          }
+          this.videoInfo = await response.data.data
+          console.log(this.videoInfo)
+          console.log(this.videoInfo.url)
+          this.platform = this.videoInfo.platform
+        }catch(err) {
+          console.log(err.response.data.detail)
+          // console.error( err.response || err)
+          this.errorMessage = err.response.data.detail || err
+        }finally {
+          this.isLoading = false
+        }
       },
 
       async handlePaste(e) {
@@ -193,7 +239,8 @@
           if (navigator.clipboard && navigator.clipboard.readText){
             const text = await navigator.clipboard.readText()
             this.videoUrl = text; 
-          }else {
+          }
+          else {
             alert("Pasting not supported in Browser")
           }
         } catch(err) {
@@ -205,6 +252,11 @@
       extractVideoId() {
         const videoIdMatch = this.videoUrl.match(/(?:v=|\/shorts\/|\/embed\/|youtu\.be\/)([a-zA-Z0-9_-]+)/);
         return videoIdMatch ? videoIdMatch[1] : ""
+      },
+
+      handleClear() {
+        this.videoInfo = {};
+        this.videoUrl = "";
       }
       
     },
@@ -212,18 +264,14 @@
     watch: {
       videoUrl(newVal) {
         if (newVal) {
-          const pattern1 = /^http:\/\/www\.|https:\/\/[a-zA-z0-9-_.]+/
+          const pattern1 = /^https?:\/\/www\.|https:\/\/[a-zA-z0-9-_.]+/
           const match = newVal.match(pattern1)
-          if(!match || newVal.length > 200){
+          if(!match || newVal.length > 100){
             this.errorMessage = "Invalid Url"
             this.videoUrl = ""
             return
-          }          
-          
-          if (newVal.includes("youtube")){
-            this.platform = 'youtube'
-          }
-          
+          } 
+          this.getVideoInfo(newVal)
         }
       },
 
