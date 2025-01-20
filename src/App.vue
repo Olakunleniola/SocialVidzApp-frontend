6dd76ba4ce3e54012cd4ed3d5ef98d3bbf269a45
devlakunle <delakunle@gmail.com>
2025-01-20 01:21:30 +0100

    Fix: Resolved a bug in the handleDownload function by removing undeclared variables causing unexpected behavior

--- src/App.vue
+++ src/App.vue
@@ -234,8 +234,6 @@
               URL.revokeObjectURL(downloadLink);
               document.body.removeChild(downloadLink);
               this.handleClear(); // Reset or clean up any state
-              this.isLoading = false
-              this.isSuccess = true;
               return;
             }
     
@@ -251,7 +249,7 @@
         } catch (err) {
           console.error("Download error:", err);
           this.errorMessage = "Failed to download video.";
-          this.isLoading = false
+          this.isDownloading = false
         }
       },
 
